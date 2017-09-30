9b380a7ae7a0db59136fed7f685d67e6cb32d130
Karol Hinz <karolhnz@gmail.com>
2017-09-30 14:38:12 +0200

    examples project added

---
+++ Exodus.Examples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Exodus.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
+        {
+            var configuration = new MigratorConfiguration(
+                "Server=(localDb)\\MSSQLLocalDB;Database=exodus.dev;Trusted_Connection=True;");
+            var migrator = new Migrator(configuration);
+            await migrator
+                .DropCreateDatabase()
+                .Migrate();
+            Console.ReadKey();
+        }
+    }
+}

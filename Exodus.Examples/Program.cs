12d98d287f594dd09017710091e1186ac279d5d2
Karol Hinz <karol.hinz@wolterskluwer.com>
2017-10-01 22:59:13 +0200

    loading migration scripts from directory and assemblies

--- Exodus.Examples/Program.cs
+++ Exodus.Examples/Program.cs
@@ -5,13 +5,19 @@ namespace Exodus.Examples
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
+        {
+            MainAsync().Wait();
+        }
+
+        static async Task MainAsync()
         {
             var configuration = new MigratorConfiguration(
                 "Server=(localDb)\\MSSQLLocalDB;Database=exodus.dev;Trusted_Connection=True;");
             var migrator = new Migrator(configuration);
             await migrator
                 .DropCreateDatabase()
+                .FromAssembly("Exodus.Migrations")
                 .LogToConsole()
                 .MigrateAsync();
             Console.ReadKey();

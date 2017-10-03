3c3886ad4e3f6eacbb2411cd7b9375aab9f6908f
Karol Hinz <karolhnz@gmail.com>
2017-10-03 20:28:16 +0200

    RunMigration command Migration property changed to private field (public property is no longer needed)

--- Exodus/Commands/RunMigration.cs
+++ Exodus/Commands/RunMigration.cs
@@ -8,12 +8,12 @@ namespace Exodus.Commands
 {
     class RunMigration : Command
     {
-        public Migration Migration { get; }
+        readonly Migration _migration;
 
         public RunMigration(string connectionString, Migration migration)
             : base(connectionString)
         {
-            Migration = migration;
+            _migration = migration;
             Sql = $@"
                 {migration.Script}
                 INSERT INTO Migrations VALUES (@version, @appliedOn, @name);
@@ -22,9 +22,9 @@ namespace Exodus.Commands
 
         protected override void AddParameters(SqlParameterCollection parameters)
         {
-            parameters.Add(new SqlParameter("version", Migration.Version));
+            parameters.Add(new SqlParameter("version", _migration.Version));
             parameters.Add(new SqlParameter("appliedOn", DateTime.Now));
-            parameters.Add(new SqlParameter("name", Migration.Name));
+            parameters.Add(new SqlParameter("name", _migration.Name));
         }
     }
 }

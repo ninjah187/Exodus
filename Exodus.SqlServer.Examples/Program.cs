a443866257312b5eefe523af63f2925cfcf3c126
Karol Hinz <karolhnz@gmail.com>
2017-12-08 18:50:07 +0100

    fix in running migrations creating stored procedures for SQL Server

--- Exodus.SqlServer.Examples/Program.cs
+++ Exodus.SqlServer.Examples/Program.cs
@@ -5,17 +5,31 @@ namespace Exodus.SqlServer.Examples
 {
     class Program
     {
-        static async Task Main(string[] args)
+        //static async Task Main(string[] args)
+        //{
+        //    var migrator = new SqlServerMigrator(
+        //        "Server=(localDb)\\MSSQLLocalDB;" +
+        //        "Database=exodus.dev;" +
+        //        "Trusted_Connection=True;");
+        //    await migrator
+        //        .DropCreateDatabase()
+        //        .FromAssembly("dsadsad")
+        //        .LogToConsole()
+        //        .MigrateAsync();
+        //    Console.ReadKey();
+        //}
+
+        static void Main(string[] args)
         {
             var migrator = new SqlServerMigrator(
                 "Server=(localDb)\\MSSQLLocalDB;" +
                 "Database=exodus.dev;" +
                 "Trusted_Connection=True;");
-            await migrator
+            migrator
                 .DropCreateDatabase()
-                .FromAssembly("dsadsad")
+                .FromAssembly("Exodus.SqlServer.Examples")
                 .LogToConsole()
-                .MigrateAsync();
+                .Migrate();
             Console.ReadKey();
         }
     }
